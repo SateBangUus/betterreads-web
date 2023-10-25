aa7d54b670797e58c23aa00004d642a8015c1880
Muhammad Oka <oka@muhammadoka.dev>
2023-10-25 18:02:04 +0700

    fix auth urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-from authentication.views import login_user, register_user
-=======
 from authentication.views import login_user, register_user, logout_user
->>>>>>> f86f652 (add logout)
 from django.urls import path
 
 app_name = 'authentication'
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register_user, name='register'),
-<<<<<<< HEAD
-=======
     path('logout/', logout_user, name='logout')
->>>>>>> f86f652 (add logout)
 ]
