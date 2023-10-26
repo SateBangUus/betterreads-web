90b8185ec085eaeeb54096e81c126fbf66af722b
Muhammad Oka <oka@muhammadoka.dev>
2023-10-26 16:16:24 +0700

    add viewing other users profile

--- user/views.py
+++ user/views.py
@@ -2,17 +2,20 @@ from django.urls import reverse
 import requests, json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from user.models import Rating
 from api.views import get_book
 
 # Create your views here.
 @login_required(login_url='/auth/login/')
-def user_profile(request):
+def user_profile(request, username):
+    user_profile = User.objects.get(username=username)
     top5ratings = [
-        {"rating": book.rating, "json_data": json.loads(get_book(request, book.book_id).content)['volumeInfo']} for book in Rating.objects.filter(user=request.user).order_by('-rating')[:5]
+        {"rating": book.rating, "json_data": json.loads(get_book(request, book.book_id).content)['volumeInfo']} for book in Rating.objects.filter(user=user_profile).order_by('-rating')[:5]
     ]
     context = {
+        "user_profile": user_profile,
         "top5ratings": top5ratings
     }
 
