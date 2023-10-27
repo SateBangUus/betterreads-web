58d49de42e144f097a171be767d2fdbc9b9e3249
ferdinandraja <ferdinand.raja@ui.ac.id>
2023-10-27 22:02:16 +0700

    add page for cart

--- buy/views.py
+++ buy/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from buy.models import Item
+from buy.models import Cart
+from book.models import Book
+from django.http import HttpResponse
+from django.core import serializers
 # Create your views here.
 @login_required(login_url='/login')
 def incrementBook(request, id):
-    data = Item.objects.get(id =id)
+    data = Cart.objects.get(id =id)
     data.amount +=1
     data.save()
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def decrementBook(request, id):
-    data = Item.objects.get(id =id)
+    data = Cart.objects.get(id =id)
     data.amount -=1
     data.save()
     return HttpResponseRedirect(reverse('main:show_main'))
@@ -22,3 +25,10 @@ def buy_book(request):#id):
     #books = data.amount
     #context = {'books': books}
     return render(request, "buy_product.html")
+def cart(request):
+    data = Cart.objects.filter(user=request.user)
+    context = {"data" : data,}
+    return render(request, "buy.html",context)
+def get_product_json(request):
+    book_item = Cart.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', book_item))
