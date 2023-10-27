91b73db333a96c5117f9b5819e69d5991bd67b28
ferdinandraja <ferdinand.raja@ui.ac.id>
2023-10-27 14:14:01 +0700

    create new buy app

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
-from book.views import incrementBook, decrementBook, buy_book
 app_name = 'book'
 urlpatterns = [
-    path('increment/<int:id>', incrementBook, name='increment'),
-    path('decrement/<int:id>', decrementBook, name='decrement'),
-    path('buy-book/', buy_book, name='buy'),
 ]
--- book/views.py
+++ book/views.py
@@ -2,22 +2,4 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from book.models import Item
 # Create your views here.
-@login_required(login_url='/login')
-def incrementBook(request, id):
-    data = Item.objects.get(id =id)
-    data.amount +=1
-    data.save()
-    return HttpResponseRedirect(reverse('main:show_main'))
-
-def decrementBook(request, id):
-    data = Item.objects.get(id =id)
-    data.amount -=1
-    data.save()
-    return HttpResponseRedirect(reverse('main:show_main'))
-def buy_book(request):#id):
-    #data = Item.objects.get(id =id)
-    #books = data.amount
-    #context = {'books': books}
-    return render(request, "buy_product.html")
