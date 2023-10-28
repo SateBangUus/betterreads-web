8ce0ec798c04a72d09e343384c28a5ae4eeca5c5
ferdinandraja <ferdinand.raja@ui.ac.id>
2023-10-28 20:46:15 +0700

    revision of buy.views for json

--- buy/views.py
+++ buy/views.py
@@ -7,19 +7,20 @@ from buy.models import Cart
 from book.models import Book
 from django.http import HttpResponse
 from django.core import serializers
+import json
 # Create your views here.
 @login_required(login_url='/login')
 def incrementBook(request, id):
-    data = Cart.objects.get(id =id)
+    data = Cart.objects.get(pk =id)
     data.amount +=1
     data.save()
-    return HttpResponseRedirect(reverse('main:show_main'))
+    return HttpResponse(b"Increment",201)
 
 def decrementBook(request, id):
-    data = Cart.objects.get(id =id)
+    data = Cart.objects.get(pk =id)
     data.amount -=1
     data.save()
-    return HttpResponseRedirect(reverse('main:show_main'))
+    return HttpResponse(b"Decrement",201)
 def buy_book(request):#id):
     #data = Item.objects.get(id =id)
     #books = data.amount
@@ -27,8 +28,26 @@ def buy_book(request):#id):
     return render(request, "buy_product.html")
 def cart(request):
     data = Cart.objects.filter(user=request.user)
-    context = {"data" : data,}
+    counter = len(data)
+    context = {"data" : data,
+               "counter": counter}
     return render(request, "buy.html",context)
 def get_product_json(request):
     book_item = Cart.objects.filter(user=request.user)
-    return HttpResponse(serializers.serialize('json', book_item))
+    temp = []
+    for book in book_item:
+        temp.append({
+            "title" : book.book.title,
+            "author" : book.book.author,
+            "publisher" : book.book.publisher,
+            "published_date" : book.book.published_date,
+            "description" : book.book.description,
+            "genre" : book.book.genre,
+            "image" : book.book.image_link,
+        })
+    finaljson = json.dumps(temp)
+    return HttpResponse(finaljson, content_type='application/json')
+def delete_book(request,id):
+    data = Cart.objects.get(pk=id)
+    data.delete()
+    return HttpResponse(b"Deleted",201)
