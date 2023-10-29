e74cc9d12d88bd5f49dfef555e9e1acfd811673b
Muhammad Oka <oka@muhammadoka.dev>
2023-10-29 21:59:42 +0700

    add edit profile functionality

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import user_profile, become_curator
+from user.views import user_profile, become_curator, edit_profile
 
 app_name = 'user'
 urlpatterns = [
     path('curator', become_curator, name='become_curator'),
     path('profile/', user_profile, name='user_profile'),
+    path('profile/edit', edit_profile, name='edit_profile'),
     path('profile/<str:username>', user_profile, name='user_profile'),
 ]
