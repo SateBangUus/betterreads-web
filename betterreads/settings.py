08eec217f5059a440989e3373421c852e9c901b7
Muhammad Oka <oka@muhammadoka.dev>
2023-10-29 23:03:55 +0700

    set allowed_hosts to any

--- betterreads/settings.py
+++ betterreads/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_=-xk7mw_(-y62y4rjsv4o%_lklyy-+eoxgz8i8a$c#!7-ve2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
